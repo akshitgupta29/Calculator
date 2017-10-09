ac0bdec173c106bce055bede00eeaa79466f9865
Akshit <akshitgupta29>
2017-10-09 18:54:15 +0530

    Initial commit

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
+            this.ultraButton7 = new Infragistics.Win.Misc.UltraButton();
+            this.Result = new Infragistics.Win.Misc.UltraLabel();
+            this.ultraButton8 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton9 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton4 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton5 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton6 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton1 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton2 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton3 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButtonSignChange = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton0 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButtonDecimal = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButtonDivide = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButtonMultiply = new Infragistics.Win.Misc.UltraButton();
+            this.UltraButtonMinus = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButtonPlus = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButtonClear = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton16 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButton17 = new Infragistics.Win.Misc.UltraButton();
+            this.ultraButtonAnswer = new Infragistics.Win.Misc.UltraButton();
+            this.SuspendLayout();
+            // 
+            // ultraButton7
+            // 
+            this.ultraButton7.Location = new System.Drawing.Point(36, 142);
+            this.ultraButton7.Name = "ultraButton7";
+            this.ultraButton7.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton7.TabIndex = 0;
+            this.ultraButton7.Text = "7";
+            this.ultraButton7.Click += new System.EventHandler(this.ultraButton7_Click);
+            // 
+            // Result
+            // 
+            this.Result.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            appearance1.BackColor = System.Drawing.Color.White;
+            appearance1.FontData.BoldAsString = "True";
+            appearance1.TextHAlignAsString = "Right";
+            appearance1.TextVAlignAsString = "Middle";
+            this.Result.Appearance = appearance1;
+            this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Result.Location = new System.Drawing.Point(36, 26);
+            this.Result.Name = "Result";
+            this.Result.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Result.Size = new System.Drawing.Size(318, 47);
+            this.Result.TabIndex = 1;
+            this.Result.Text = "0";
+            // 
+            // ultraButton8
+            // 
+            this.ultraButton8.Location = new System.Drawing.Point(117, 142);
+            this.ultraButton8.Name = "ultraButton8";
+            this.ultraButton8.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton8.TabIndex = 2;
+            this.ultraButton8.Text = "8";
+            this.ultraButton8.Click += new System.EventHandler(this.ultraButton8_Click);
+            // 
+            // ultraButton9
+            // 
+            this.ultraButton9.Location = new System.Drawing.Point(198, 142);
+            this.ultraButton9.Name = "ultraButton9";
+            this.ultraButton9.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton9.TabIndex = 3;
+            this.ultraButton9.Text = "9";
+            this.ultraButton9.Click += new System.EventHandler(this.ultraButton9_Click);
+            // 
+            // ultraButton4
+            // 
+            this.ultraButton4.Location = new System.Drawing.Point(36, 171);
+            this.ultraButton4.Name = "ultraButton4";
+            this.ultraButton4.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton4.TabIndex = 4;
+            this.ultraButton4.Text = "4";
+            this.ultraButton4.Click += new System.EventHandler(this.ultraButton4_Click);
+            // 
+            // ultraButton5
+            // 
+            this.ultraButton5.Location = new System.Drawing.Point(117, 171);
+            this.ultraButton5.Name = "ultraButton5";
+            this.ultraButton5.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton5.TabIndex = 5;
+            this.ultraButton5.Text = "5";
+            this.ultraButton5.Click += new System.EventHandler(this.ultraButton5_Click);
+            // 
+            // ultraButton6
+            // 
+            this.ultraButton6.Location = new System.Drawing.Point(198, 171);
+            this.ultraButton6.Name = "ultraButton6";
+            this.ultraButton6.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton6.TabIndex = 6;
+            this.ultraButton6.Text = "6";
+            this.ultraButton6.Click += new System.EventHandler(this.ultraButton6_Click);
+            // 
+            // ultraButton1
+            // 
+            this.ultraButton1.Location = new System.Drawing.Point(36, 200);
+            this.ultraButton1.Name = "ultraButton1";
+            this.ultraButton1.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton1.TabIndex = 7;
+            this.ultraButton1.Text = "1";
+            this.ultraButton1.Click += new System.EventHandler(this.ultraButton1_Click);
+            // 
+            // ultraButton2
+            // 
+            this.ultraButton2.Location = new System.Drawing.Point(117, 200);
+            this.ultraButton2.Name = "ultraButton2";
+            this.ultraButton2.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton2.TabIndex = 8;
+            this.ultraButton2.Text = "2";
+            this.ultraButton2.Click += new System.EventHandler(this.ultraButton2_Click);
+            // 
+            // ultraButton3
+            // 
+            this.ultraButton3.Location = new System.Drawing.Point(198, 200);
+            this.ultraButton3.Name = "ultraButton3";
+            this.ultraButton3.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton3.TabIndex = 9;
+            this.ultraButton3.Text = "3";
+            this.ultraButton3.Click += new System.EventHandler(this.ultraButton3_Click);
+            // 
+            // ultraButtonSignChange
+            // 
+            this.ultraButtonSignChange.ImageTransparentColor = System.Drawing.Color.Turquoise;
+            this.ultraButtonSignChange.Location = new System.Drawing.Point(36, 229);
+            this.ultraButtonSignChange.Name = "ultraButtonSignChange";
+            this.ultraButtonSignChange.Size = new System.Drawing.Size(75, 23);
+            this.ultraButtonSignChange.TabIndex = 10;
+            this.ultraButtonSignChange.Text = "- (Sign)";
+            // 
+            // ultraButton0
+            // 
+            this.ultraButton0.Location = new System.Drawing.Point(117, 229);
+            this.ultraButton0.Name = "ultraButton0";
+            this.ultraButton0.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton0.TabIndex = 11;
+            this.ultraButton0.Text = "0";
+            this.ultraButton0.Click += new System.EventHandler(this.ultraButton0_Click);
+            // 
+            // ultraButtonDecimal
+            // 
+            this.ultraButtonDecimal.Location = new System.Drawing.Point(198, 229);
+            this.ultraButtonDecimal.Name = "ultraButtonDecimal";
+            this.ultraButtonDecimal.Size = new System.Drawing.Size(75, 23);
+            this.ultraButtonDecimal.TabIndex = 12;
+            this.ultraButtonDecimal.Text = ".";
+            // 
+            // ultraButtonDivide
+            // 
+            this.ultraButtonDivide.Location = new System.Drawing.Point(279, 142);
+            this.ultraButtonDivide.Name = "ultraButtonDivide";
+            this.ultraButtonDivide.Size = new System.Drawing.Size(75, 23);
+            this.ultraButtonDivide.TabIndex = 13;
+            this.ultraButtonDivide.Text = "/";
+            // 
+            // ultraButtonMultiply
+            // 
+            this.ultraButtonMultiply.Location = new System.Drawing.Point(279, 171);
+            this.ultraButtonMultiply.Name = "ultraButtonMultiply";
+            this.ultraButtonMultiply.Size = new System.Drawing.Size(75, 23);
+            this.ultraButtonMultiply.TabIndex = 14;
+            this.ultraButtonMultiply.Text = "*";
+            // 
+            // UltraButtonMinus
+            // 
+            this.UltraButtonMinus.Location = new System.Drawing.Point(279, 200);
+            this.UltraButtonMinus.Name = "UltraButtonMinus";
+            this.UltraButtonMinus.Size = new System.Drawing.Size(75, 23);
+            this.UltraButtonMinus.TabIndex = 15;
+            this.UltraButtonMinus.Text = "-";
+            // 
+            // ultraButtonPlus
+            // 
+            this.ultraButtonPlus.Location = new System.Drawing.Point(279, 229);
+            this.ultraButtonPlus.Name = "ultraButtonPlus";
+            this.ultraButtonPlus.Size = new System.Drawing.Size(75, 23);
+            this.ultraButtonPlus.TabIndex = 16;
+            this.ultraButtonPlus.Text = "+";
+            this.ultraButtonPlus.Click += new System.EventHandler(this.ultraButtonPlus_Click);
+            // 
+            // ultraButtonClear
+            // 
+            this.ultraButtonClear.Location = new System.Drawing.Point(36, 99);
+            this.ultraButtonClear.Name = "ultraButtonClear";
+            this.ultraButtonClear.Size = new System.Drawing.Size(75, 23);
+            this.ultraButtonClear.TabIndex = 17;
+            this.ultraButtonClear.Text = "Clear";
+            this.ultraButtonClear.Click += new System.EventHandler(this.ultraButtonClear_Click);
+            // 
+            // ultraButton16
+            // 
+            this.ultraButton16.Location = new System.Drawing.Point(117, 99);
+            this.ultraButton16.Name = "ultraButton16";
+            this.ultraButton16.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton16.TabIndex = 18;
+            this.ultraButton16.Text = "7";
+            // 
+            // ultraButton17
+            // 
+            this.ultraButton17.Location = new System.Drawing.Point(198, 99);
+            this.ultraButton17.Name = "ultraButton17";
+            this.ultraButton17.Size = new System.Drawing.Size(75, 23);
+            this.ultraButton17.TabIndex = 19;
+            this.ultraButton17.Text = "7";
+            // 
+            // ultraButtonAnswer
+            // 
+            this.ultraButtonAnswer.Location = new System.Drawing.Point(36, 276);
+            this.ultraButtonAnswer.Name = "ultraButtonAnswer";
+            this.ultraButtonAnswer.Size = new System.Drawing.Size(318, 23);
+            this.ultraButtonAnswer.TabIndex = 20;
+            this.ultraButtonAnswer.Text = "Answer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 346);
+            this.Controls.Add(this.ultraButtonAnswer);
+            this.Controls.Add(this.ultraButton17);
+            this.Controls.Add(this.ultraButton16);
+            this.Controls.Add(this.ultraButtonClear);
+            this.Controls.Add(this.ultraButtonPlus);
+            this.Controls.Add(this.UltraButtonMinus);
+            this.Controls.Add(this.ultraButtonMultiply);
+            this.Controls.Add(this.ultraButtonDivide);
+            this.Controls.Add(this.ultraButtonDecimal);
+            this.Controls.Add(this.ultraButton0);
+            this.Controls.Add(this.ultraButtonSignChange);
+            this.Controls.Add(this.ultraButton3);
+            this.Controls.Add(this.ultraButton2);
+            this.Controls.Add(this.ultraButton1);
+            this.Controls.Add(this.ultraButton6);
+            this.Controls.Add(this.ultraButton5);
+            this.Controls.Add(this.ultraButton4);
+            this.Controls.Add(this.ultraButton9);
+            this.Controls.Add(this.ultraButton8);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.ultraButton7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.Misc.UltraButton ultraButton7;
+        private Infragistics.Win.Misc.UltraLabel Result;
+        private Infragistics.Win.Misc.UltraButton ultraButton8;
+        private Infragistics.Win.Misc.UltraButton ultraButton9;
+        private Infragistics.Win.Misc.UltraButton ultraButton4;
+        private Infragistics.Win.Misc.UltraButton ultraButton5;
+        private Infragistics.Win.Misc.UltraButton ultraButton6;
+        private Infragistics.Win.Misc.UltraButton ultraButton1;
+        private Infragistics.Win.Misc.UltraButton ultraButton2;
+        private Infragistics.Win.Misc.UltraButton ultraButton3;
+        private Infragistics.Win.Misc.UltraButton ultraButtonSignChange;
+        private Infragistics.Win.Misc.UltraButton ultraButton0;
+        private Infragistics.Win.Misc.UltraButton ultraButtonDecimal;
+        private Infragistics.Win.Misc.UltraButton ultraButtonDivide;
+        private Infragistics.Win.Misc.UltraButton ultraButtonMultiply;
+        private Infragistics.Win.Misc.UltraButton UltraButtonMinus;
+        private Infragistics.Win.Misc.UltraButton ultraButtonPlus;
+        private Infragistics.Win.Misc.UltraButton ultraButtonClear;
+        private Infragistics.Win.Misc.UltraButton ultraButton16;
+        private Infragistics.Win.Misc.UltraButton ultraButton17;
+        private Infragistics.Win.Misc.UltraButton ultraButtonAnswer;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+       // static string operatorToDo;
+        double operand1 = 0.0;
+        double operand2 = 0.0;
+        char operation = '\0';
+        double resultVariable = 0;
+        bool buttoonPress = false;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            //operand1 = Convert.ToInt32(Result.Text);
+            //operand2 = Convert.ToInt32(Result.Text);
+            //Result.Text = "0";
+        }
+
+        private void ultraButton2_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "2";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "2";
+            }
+            
+        }
+
+        private void ultraButton1_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "1";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "1";
+            }
+            
+        }
+
+        public void ClearScreen()
+        {
+            Result.Text = "";
+            //operation = '\0';
+            //operand1 = 0;
+            //operand2 = 0;
+
+        }
+        public void clearOperation()
+        {
+            operand2 = 0;
+            operand1 = 0;
+            operation = '\0';
+        }
+
+        private void ultraButton3_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "3";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "3";
+            }
+            
+        }
+
+        private void ultraButton4_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "4";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "4";
+            }
+            
+        }
+
+        private void ultraButton5_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "5";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "5";
+            }
+            
+        }
+
+        private void ultraButton6_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "6";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "6";
+            }
+            
+        }
+
+        private void ultraButton7_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "7";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "7";
+            }
+            
+        }
+
+        private void ultraButton8_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "8";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "8";
+            }
+            
+        }
+
+        private void ultraButton9_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "9";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "9";
+            }
+            
+        }
+
+        private void ultraButton0_Click(object sender, EventArgs e)
+        {
+            if (buttoonPress == false)
+            {
+                ClearScreen();
+                Result.Text = "0";
+                buttoonPress = true;
+            }
+            else
+            {
+                Result.Text = Result.Text + "0";
+            }
+            
+        }
+
+        private void ultraButtonClear_Click(object sender, EventArgs e)
+        {
+            
+            ClearScreen();
+            clearOperation();
+            Result.Text = "0";
+            buttoonPress = false;
+        }
+
+        private void ultraButtonPlus_Click(object sender, EventArgs e)
+        {
+            operand2 = Convert.ToDouble(Result.Text);
+            
+            if (operation == '\0')
+            {
+                operand1 = operand2;
+                resultVariable = operand1;
+            }
+            else
+            {
+                resultVariable = operand1 + operand2;
+                operand1 = resultVariable;
+            }
+            operation = '+';
+            buttoonPress = false;
+            Result.Text = resultVariable.ToString();
+
+            //ClearScreen();
+            //bool buttonplus = false;
+            //operand2 =  Convert.ToDouble(Result.Text);
+            //if (operation == ' ')
+            //{
+            //    //operand2 =  Convert.ToDouble(Result.Text);
+               
+            //    operand1 = operand2;
+            //}
+            //else
+            //{
+            //    resultVariable= operand1 + operand2;
+            //    operand1 = resultVariable;
+            //}
+
+            //ClearScreen();
+            //Result.Text = "0";
+           // int operand2 = Convert.ToInt32(Result.Text);
+            //int result = operand1 + operand2;
+            //Result.Text = resultVariable.ToString();
+
+            
+        }
+
+        
+        
+
+        
+     
+    }
+}
